8b13b1bc6f0f63efe0206b1e536f4d403e66b827
NguyenVietAnh74072 <anh74072@st.vimaru.edu.vn>
2020-09-22 14:41:32 +0700

    2.41PM 9/22/2020

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.scss";
-import ColorBox from "./components/ColorBox/indenx";
+import React, { Fragment, useState } from "react";
 
 function App() {
+  const [ma_doi_tuong, SetMaLoaiDoiTuong] = useState([]);
+  const [ten_doi_tuong, SetTenDoiTuong] = useState([]);
+  const data = [
+    {
+      ma_doi_tuong: "LDT0001",
+      ten_doi_tuong: "A",
+    },
+    {
+      ma_doi_tuong: "LDT0002",
+      ten_doi_tuong: "B",
+    },
+  ];
+  const HanderChangeMaDoiTuong = (e) => {
+    alert(e.target.value);
+  };
   return (
-    <div className="app">
-      <h1>helo the gioi</h1>
-      <ColorBox />
-    </div>
+    <Fragment>
+      <form>
+        <span>ma_doi_tuong</span>
+        <input
+          type="text"
+          onchange={async (e) => HanderChangeMaDoiTuong(e)}
+        ></input>
+        <br></br>
+
+        <span>ten_doi_tuong</span>
+        <input
+          type="text"
+          onchange={(e) => alert(e.target.value)}
+          value={ten_doi_tuong}
+        />
+        <br></br>
+        <button onClick={async () => console.log(ten_doi_tuong)}>Luu</button>
+        <span> Danh Sach </span>
+        {data.map((x) => (
+          <Fragment>
+            <div> Ten doi tuong : {x.ten_doi_tuong}</div>
+            <div> Ma doi tuong : {x.ma_doi_tuong} </div>
+          </Fragment>
+        ))}
+      </form>
+    </Fragment>
   );
 }
 
--- src/components/ColorBox/indenx.jsx
+++ src/components/ColorBox/indenx.jsx
@@ -1,27 +1,51 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
-ColorBox.propTypes = {};
-function getRandomColor() {
+function ColorBox() {
+  const data = [
+    {
+      ma_doi_tuong: "LDT0001",
+      ten_doi_tuong: "A",
+    },
+    {
+      ma_doi_tuong: "LDT0002",
+      ten_doi_tuong: "B",
+    },
+  ];
+  const [ma_doi_tuong, SetMaLoaiDoiTuong] = useState([]);
+  const [ten_doi_tuong, SetTenDoiTuong] = useState([]);
+  const [index, Setindex] = useState(0);
   const Color_list = ["black", "blue", "green", "yellow", "deeppink"];
-  const RandomIndex = Math.trunc(Math.random() * 5);
-  return Color_list[RandomIndex];
-}
-
-function ColorBox(props) {
   const [color, setColor] = useState("green");
-  function HandeColorBox() {
+  const HandeColorBox = async () => {
     // get randome color ->setColor
-    const newColor = getRandomColor();
-    setColor(newColor);
-  }
+    index != 4 ? Setindex(index + 1) : Setindex(0);
+    setColor(Color_list[index]);
+    console.log(index);
+  };
+  const a = () => {};
   return (
-    <div
-      className="color-box"
-      style={{ backgroundColor: color }}
-      onclick={HandeColorBox}
-    >
-      ColorBox
-    </div>
+    <Fragment>
+      <span>ma_doi_tuong</span>
+      <input type="text" onchange={async (e) => alert(e.target.value)}></input>
+      <br></br>
+
+      <span>ten_doi_tuong</span>
+      <input
+        type="text"
+        onchange={async (key) => {
+          console.log(key.target.value);
+        }}
+      ></input>
+      <br></br>
+      <button onClick={async () => console.log(ma_doi_tuong)}>Luu</button>
+      <span> Danh Sach </span>
+      {data.map((x) => (
+        <Fragment>
+          <div> Ten doi tuong : {x.ten_doi_tuong}</div>
+          <div> Ma doi tuong : {x.ma_doi_tuong} </div>
+        </Fragment>
+      ))}
+    </Fragment>
   );
 }
 
